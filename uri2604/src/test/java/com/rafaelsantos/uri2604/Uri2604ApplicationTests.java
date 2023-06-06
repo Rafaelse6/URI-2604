d9a35bbfca7d64d0445907d1fce8026a230f41cb
Rafael Santos <rafa.05125@gmail.com>
2023-06-06 19:36:14 -0300

    Project created

---
+++ uri2604/src/test/java/com/rafaelsantos/uri2604/Uri2604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.uri2604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
