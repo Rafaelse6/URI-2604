b8362972669f17a4d05333938f3becffb5b11f30
Rafael Santos <rafa.05125@gmail.com>
2023-06-06 19:39:27 -0300

    Product entity

---
+++ uri2604/src/main/java/com/rafaelsantos/uri2604/entities/Product.java
@@ -0,0 +1,79 @@
+package com.rafaelsantos.uri2604.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	private Long id;
+
+	private String name;
+	private Integer amount;
+	private Double price;
+
+	public Product() {
+	}
+
+	public Product(Long id, String name, Integer amount, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+}
